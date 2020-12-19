a6d3cc9b307fde2ec2d35f75056c94db92464c92
wkdqls00 <wkdqls00@naver.com>
2020-12-20 01:05:13 +0900

    Django Project

---
+++ bookmark/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+
+class Bookmark(models.Model):
+    site_name = models.CharField(max_length=100)
+    url = models.URLField('site URL')
+
+    def get_absolute_url(self):
+        return reverse('bookmark:detail', args = [str(self.id)])
+
+    def __str__(self):
+        # 객체를 출력할 때 나타날 값
+        return "이름 : " + self.site_name + ", 주소 : " + self.url
